36526cd1cc062dfaa924bc1d066bc1fe9d9e0c9c
Abhishek Bhandari <abhishek.bhandari@atos.net>
2021-05-08 16:05:26 +0530

    changed the get api to match the data format

--- Backend/stock_data/views.py
+++ Backend/stock_data/views.py
@@ -13,12 +13,31 @@ class Real_Time_Stock_Data(APIView):
         self.nse = Nse()
 
     def get(self, request):
-
+        stock_list = []
         dataframe = pd.read_csv(os.path.join(settings.BASE_DIR, 'stock_list.csv'))
         head = dataframe.head()
-        stock_dict = dict(zip(list(head['SYMBOL']), list(head['NAME OF COMPANY'])))
-        print(stock_dict)
-        return JsonResponse({"data": stock_dict})
+        for index, row in head.iterrows():
+            stock_dict = {}
+            stock_dict['id'] = index + 1
+            stock_dict['name'] = row['NAME OF COMPANY']
+            stock_dict['index_nm'] = row['SYMBOL']
+            stock_dict['currency'] = "INR"
+            stock_dict["market"] = "NSE"
+            stock_value = self.nse.get_quote(row['SYMBOL'])
+            # stock_value = self.nse.get_quote(row)
+            stock_dict['value'] = str(stock_value['lastPrice'])
+            if isinstance(stock_value['pChange'], str) and stock_value['pChange'].find('-') != -1:
+                stock_dict['state'] = '-'
+                stock_dict['state_value'] = str(stock_value['change'])
+                stock_dict['percent_state'] = str(stock_value['pChange']) + "%"
+            else:
+                stock_dict['state'] = "+"
+                stock_dict['state_value'] = str(stock_value['change'])
+                stock_dict['percent_state'] = str(stock_value['pChange']) + "%" 
+            
+            stock_list.append(stock_dict)
+
+        return JsonResponse({"data": stock_list})
 		
     def post(self, request):
         
