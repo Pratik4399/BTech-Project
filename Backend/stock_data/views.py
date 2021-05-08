2896ef1d3c1c06b477e354c4c72abbc7b229f4f7
Abhishek Bhandari <abhishek.bhandari@atos.net>
2021-05-08 16:15:15 +0530

    Added Nifty 50 Stocks

--- Backend/stock_data/views.py
+++ Backend/stock_data/views.py
@@ -15,15 +15,15 @@ class Real_Time_Stock_Data(APIView):
     def get(self, request):
         stock_list = []
         dataframe = pd.read_csv(os.path.join(settings.BASE_DIR, 'stock_list.csv'))
-        head = dataframe.head()
-        for index, row in head.iterrows():
+        # head = dataframe.head()
+        for index, row in dataframe.iterrows():
             stock_dict = {}
             stock_dict['id'] = index + 1
-            stock_dict['name'] = row['NAME OF COMPANY']
-            stock_dict['index_nm'] = row['SYMBOL']
+            stock_dict['name'] = row['Company Name']
+            stock_dict['index_nm'] = row['Symbol']
             stock_dict['currency'] = "INR"
             stock_dict["market"] = "NSE"
-            stock_value = self.nse.get_quote(row['SYMBOL'])
+            stock_value = self.nse.get_quote(row['Symbol'])
             # stock_value = self.nse.get_quote(row)
             stock_dict['value'] = str(stock_value['lastPrice'])
             if isinstance(stock_value['pChange'], str) and stock_value['pChange'].find('-') != -1:
